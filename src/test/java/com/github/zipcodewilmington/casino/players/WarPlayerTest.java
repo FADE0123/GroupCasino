dc88acd1d1bee0c1d44103fb0cceb32d2f1a0613
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-17 22:21:10 -0500

    Add War, WarPlayer, WarPlayerTest

---
+++ src/test/java/com/github/zipcodewilmington/casino/players/WarPlayerTest.java
@@ -0,0 +1,58 @@
+package com.github.zipcodewilmington.casino.players;
+
+import com.github.zipcodewilmington.Cards;
+import com.github.zipcodewilmington.Deck;
+import com.github.zipcodewilmington.Suits;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarPlayerTest {
+
+    @Test
+    public void emptyHandTest(){
+        WarPlayer player = new WarPlayer();
+        Cards card = new Cards(Suits.SPADES, 1);
+        player.draw(card);
+        assertEquals(1, player.getNumberCards());
+        player.emptyHand();
+        assertEquals(0, player.getNumberCards());
+    }
+    @Test
+    public void drawTest(){
+        WarPlayer player = new WarPlayer();
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        assertEquals(1, player.getNumberCards());
+    }
+    @Test
+    public void getHandSumTest(){
+        WarPlayer player = new WarPlayer();
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        player.draw(deck.dealCard());
+        player.playerCardReveal(true);
+        assertEquals(13, player.getHandSum());
+    }
+    @Test
+    public void getNameTest(){
+        WarPlayer player = new WarPlayer();
+        String expected = "Player";
+        assertEquals(player.getname(), expected);
+    }
+    @Test
+    public void getHandTest(){
+        WarPlayer player = new WarPlayer();
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        assertEquals("Ace of CLUBS", player.getHand(0).toString());
+    }
+    @Test
+    public void getNumberCardsTest(){
+        WarPlayer player = new WarPlayer();
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        assertEquals(1, player.getNumberCards());
+    }
+}
